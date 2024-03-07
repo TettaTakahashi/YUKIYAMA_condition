f4dc3ccd8c3de8f2c163cf1ed0320663282a67bd
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-03-07 06:30:27 +0000

    [Update]都道府県一覧・追加画面

--- app/controllers/admin/prefecture_controller.rb
+++ app/controllers/admin/prefecture_controller.rb
@@ -1,18 +1,32 @@
 class Admin::PrefectureController < ApplicationController
     
     def index
-        
+      @prefectures=Prefecture.all
+      @prefecture=Prefecture.new
     end
     
     def edit
-        
+      
     end
     
     def create
-        
+      @prefecture=Prefecture.new(prefecture_params)
+      if @prefecture.save
+        redirect_to admin_prefecture_index_path
+      else
+        @prefectures=Prefecture.all
+        redirect_to admin_prefecture_index_path
+      end
     end
     
     def update
-        
+      
+    end
+    
+    
+    private
+    
+    def prefecture_params
+      params.require(:prefecture).permit(:prefecture_name)
     end
 end
