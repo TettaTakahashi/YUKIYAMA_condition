bb089c014d530272edf78444f0eb79b3b52c24a6
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-03-08 10:27:26 +0000

    [Update]adminスキー場一覧画面

--- app/controllers/admin/ski_resorts_controller.rb
+++ app/controllers/admin/ski_resorts_controller.rb
@@ -1,2 +1,6 @@
 class Admin::SkiResortsController < ApplicationController
+    
+  def index
+    @ski_resorts=SkiResort.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :prefecture, only: [:index, :edit, :create, :update]
+    get 'ski_resort', to: 'ski_resorts#index'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
