44260ea8255ec31047518ebe1baf39b9e33916bf
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-03-11 02:16:07 +0000

    [Update]ownerスキー場一覧画面

--- app/controllers/owner/ski_resorts_controller.rb
+++ app/controllers/owner/ski_resorts_controller.rb
@@ -1,2 +1,26 @@
 class Owner::SkiResortsController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def new
+    @ski_resort=SkiResort.new
+  end
+  
+  def create
+    @ski_resort=SkiResort.new(ski_resort_params)
+    @ski_resort.save
+    redirect_to ski_resort_path(@ski_resort)
+  end
+  
+  def show
+      
+  end
+  
+  private
+  
+  def ski_resort_params
+    params.require(:ski_resort).permit(:image, :name, :introduction, :sales_active, :snow_active, :waiting_time, :price, :road_condition, :weather, :prefecture_id)
+  end
 end
