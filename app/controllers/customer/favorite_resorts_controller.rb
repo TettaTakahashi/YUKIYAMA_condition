c6138877dd64ad772d003b5da426e787ea0f774b
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-03-22 13:56:55 +0000

    [Fix]ブックマーク機能（controller,view）

--- app/controllers/customer/favorite_resorts_controller.rb
+++ app/controllers/customer/favorite_resorts_controller.rb
@@ -2,18 +2,18 @@ class Customer::FavoriteResortsController < ApplicationController
   before_action :authenticate_customer!
     
   def create
-    @ski_resort_id=SkiResort.find(params[:id])
-    @favorite_resort=current_customer.favorite_resorts.new(ski_resort_id: @ski_resort_id)
-    if @favorite_resort.save!
-      redirect_to ski_resort_path(@ski_resort_id)
+    @ski_resort=SkiResort.find(params[:id])
+    @favorite_resort=current_customer.favorite_resorts.new(ski_resort: @ski_resort)
+    if @favorite_resort.save
+      redirect_to ski_resort_path(@ski_resort)
     end
   end
   
   def destroy
-    @favorite_resort=FavoriteResort.find(params[:id])
     @ski_resort=SkiResort.find(params[:id])
-    if @favorite_resort.destroy
-      redirect_to ski_resort_path(@ski_resort)
+    favorite_resort=@ski_resort.favorite_resorts.find_by(customer_id: current_customer.id)
+    if favorite_resort.destroy
+      redirect_to favorite_resorts_path
     end
   end
   
