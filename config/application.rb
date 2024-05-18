9a7172cb7d266b6f2fe64e1ee329eaff8856595f
TettaTakahashi <x4tetta@gmail.com>
2024-05-18 09:23:59 +0000

    [Finish]YUKIYAMA_Condition

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,12 @@ module YUKIYAMACondition
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-  　config.time_zone = 'Tokyo'
+config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
   end
